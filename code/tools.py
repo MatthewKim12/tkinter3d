b6dc6cc7cd4c1596016d91130b9726a9089d13c9
MatthewKim12 <kimdjn12@gmail.com>
2025-09-13 14:50:44 -0700

    Update tools.py

--- code/tools.py
+++ code/tools.py
@@ -83,3 +83,57 @@ def rotate_z(
         y_new = x * math.sin(angle) + y * math.cos(angle)
         new_dots[id] = (x_new, y_new, z)
     return new_dots
+
+def read_obj(fp: str | bytes) -> Tuple[Dict[int, Tuple[float, float, float]], Dict[int, Tuple[int, int, int]]]:
+    """
+    reads a .obj file and finds dots (vertex) and lines
+
+    only supports vertexes (v), faces (f), and lines (l)
+    """
+    if isinstance(fp, bytes):
+        fp = fp.decode()
+    f = open(fp, "r")
+    read = f.split("\n")
+    vertexes = {}
+    lines = []
+    for line in read:
+        if not line.startswith(("v ", "f ", "l ")):
+            continue
+        if line.startswith("v "):
+            x, y, z = list(map(float, line[2:].split(" ")))
+            vertexes[len(vertexes)] = (x, y, z)
+        elif line.startswith("l "):
+            dots_ids = map(int, line[2:].split(" "))
+            for i in range(len(dots_ids) - 1):
+                lines.append((dots_ids[i], dots_ids[i+1]))
+        elif line.startswith("f "):
+            dots_ids = map(int, line[2:].split(" "))
+            for i in range(len(dots_ids) - 1):
+                lines.append((dots_ids[i], dots_ids[i+1]))
+            lines.append((dots_ids[-1], dots_ids[0]))
+    f.close()
+    return vertexes, lines
+
+def write_obj(fp: str | bytes, dots: Dict[int, Tuple[float, float, float]], lines: Dict[int, Tuple[int, int]], *, exist_ok: bool=False) -> None:
+    """
+    writes a .obj file from dots and lines
+
+    contains vertexes (v) and lines (l)
+    """
+
+    try:
+        open(fp).close()
+    except FileNotFoundError:
+        pass
+    else:
+        if not exist_ok:
+            raise FileExistsError(f"File {fp} already exists.")
+
+    if isinstance(fp, bytes):
+        fp = fp.decode()
+    f = open(fp, "w")
+    for dot in dots.values():
+        f.write(f"v {dot[0]} {dot[1]} {dot[2]}\n")
+    for line in lines.values():
+        f.write(f"l {' '.join(map(str, line))}\n")
+    f.close()
